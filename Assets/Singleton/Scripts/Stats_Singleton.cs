1b560798af50d2039787c448d5cc3c41cc0b9aa8
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-11-11 09:25:23 -0500

    fix: addition of namespaces to avoid naming conflicts as each pattern will have a similar naming structure

--- Assets/Singleton/Scripts/Stats_Singleton.cs
+++ Assets/Singleton/Scripts/Stats_Singleton.cs
@@ -1,24 +1,33 @@
 
 using UnityEngine;
 
-public class Stats_Singleton: MonoBehaviour
-{
-    private static Stats_Singleton _uniqueInstance;
+namespace dev.MikeQ.Singleton {
+    public class Stats_Singleton : MonoBehaviour
+    {
+        private static Stats_Singleton _uniqueInstance;
 
-    public static Stats_Singleton getInstance {
-        get {
-            if (!_uniqueInstance) {
-                _uniqueInstance = new GameObject().AddComponent<Stats_Singleton>();
-                _uniqueInstance.name = _uniqueInstance.GetType().ToString();
-                DontDestroyOnLoad(_uniqueInstance.gameObject);
+        public static Stats_Singleton getInstance
+        {
+            get
+            {
+                if (!_uniqueInstance)
+                {
+                    _uniqueInstance = new GameObject().AddComponent<Stats_Singleton>();
+                    _uniqueInstance.name = _uniqueInstance.GetType().ToString();
+                    DontDestroyOnLoad(_uniqueInstance.gameObject);
+                }
+                return _uniqueInstance;
             }
-            return _uniqueInstance; }
+        }
+        int _coins;
+        public void UpdateCoins(int num)
+        {
+            _coins += num;
+        }
+        public int GetCoins()
+        {
+            return _coins;
+        }
     }
-    int _coins;
-    public void UpdateCoins(int num) {
-        _coins += num;
-    }
-    public int GetCoins() {
-        return _coins;
-    }
+
 }
--- Assets/Singleton/Scripts/UIManager.cs
+++ Assets/Singleton/Scripts/UIManager.cs
@@ -2,24 +2,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
-{
-    [SerializeField] Text _scoreText, _sceneText;
-    Stats_Singleton _statInstance;
-    private void Start()
+namespace dev.MikeQ.Singleton {
+    public class UIManager : MonoBehaviour
     {
-        _statInstance = Stats_Singleton.getInstance;
-        UpdateText(_scoreText, _statInstance.GetCoins().ToString());
-        UpdateText(_sceneText, SceneManager.GetActiveScene().buildIndex.ToString());
-    }
-    public void AddCoins() {
-        _statInstance.UpdateCoins(1);
-        UpdateText(_scoreText, _statInstance.GetCoins().ToString());
-    }
-    private void UpdateText(Text textbox, string textValue) {
-        textbox.text = textValue;
-    }
-    public void NextScene() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        [SerializeField] Text _scoreText, _sceneText;
+        Stats_Singleton _statInstance;
+        private void Start()
+        {
+            _statInstance = Stats_Singleton.getInstance;
+            UpdateText(_scoreText, _statInstance.GetCoins().ToString());
+            UpdateText(_sceneText, SceneManager.GetActiveScene().buildIndex.ToString());
+        }
+        public void AddCoins()
+        {
+            _statInstance.UpdateCoins(1);
+            UpdateText(_scoreText, _statInstance.GetCoins().ToString());
+        }
+        private void UpdateText(Text textbox, string textValue)
+        {
+            textbox.text = textValue;
+        }
+        public void NextScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
+
