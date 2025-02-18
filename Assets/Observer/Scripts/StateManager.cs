ae2146cbe2c55d66ff4afe2dd483072364bf5949
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-02-17 20:01:37 -0500

    implemented interface
    As the subject, when state changes it will alert all the observers

--- Assets/Observer/Scripts/StateManager.cs
+++ Assets/Observer/Scripts/StateManager.cs
@@ -1,6 +1,55 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StateManager : MonoBehaviour
+namespace DesignPatterns.Observer
 {
-    private bool _gameIsPaused = false;
+    public class StateManager : MonoBehaviour, ISubject
+    {
+        private bool _gameIsPaused = false;
+        List<IObserver> _observers = new List<IObserver>();
+
+        [SerializeField] private Button button;
+
+        void OnEnable()
+        {
+            if (button != null)
+            {
+                button.onClick.AddListener(ToggleGameState);
+            }
+        }
+
+        void OnDisable()
+        {
+            if (button != null)
+            {
+                button.onClick.RemoveListener(ToggleGameState);
+            }
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void UnregisterObserver(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.UpdateGameState(_gameIsPaused);
+            }
+        }
+
+        void ToggleGameState()
+        {
+            _gameIsPaused = !_gameIsPaused;
+            NotifyObservers();
+        }
+    }
 }
