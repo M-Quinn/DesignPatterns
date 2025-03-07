2c10eae70fe273dd1302c2afb1e54659a8bf8711
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-06 21:57:02 -0500

    scene driver

---
+++ Assets/Template/SceneController.cs
@@ -0,0 +1,33 @@
+using DesignPatterns.Template;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if (hit.collider.transform.parent == null)
+                return;
+
+            if(hit.collider.transform.parent.TryGetComponent<Interactable>(out var interactable))
+            {
+                if (!interactable.IsInteractable())
+                    return;
+
+                if(Input.GetMouseButtonDown(0))
+                    interactable.PerformInteraction();
+            }
+        }
+    }
+
+}
