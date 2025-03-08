fac1c21dd9cc2ff06db0a5cd0ab09b4dc078e367
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-08 12:44:44 -0500

    created some of the base classes

---
+++ Assets/Iterator/Scripts/Inventory.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatterns.Iterator
+{
+    public class Inventory
+    {
+        public List<Item> Items = new List<Item>();
+        public Inventory()
+        {
+
+        }
+    }
+}
---
+++ Assets/Iterator/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace DesignPatterns.Iterator
+{
+    public class Item
+    {
+        public string Name;
+        public Item(string name)
+        {
+            Name = name;
+        }
+    }
+}
