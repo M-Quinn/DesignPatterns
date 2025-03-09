ef05d9be785b1aa848b21797dd989c061607f758
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-09 16:36:20 -0400

    removed namespaces

--- Assets/Composite/Scripts/Item.cs
+++ Assets/Composite/Scripts/Item.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace DesignPatterns.Composite
 {
     public class Item : ItemComponent
--- Assets/Composite/Scripts/ItemComponent.cs
+++ Assets/Composite/Scripts/ItemComponent.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace DesignPatterns.Composite
