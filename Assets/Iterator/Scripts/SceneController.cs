0e218421ae1ece6287f82273d0323acd417a35d9
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-09 10:27:32 -0400

    updated scene controller to use the pattern

--- Assets/Iterator/Scripts/SceneController.cs
+++ Assets/Iterator/Scripts/SceneController.cs
@@ -13,16 +13,11 @@ namespace DesignPatterns.Iterator
         [SerializeField] Button _getInventoryButton;
         [SerializeField] TMP_Text _outputTextbox;
 
-        Inventory _inventory = new Inventory();
-        private SpellBook _spellBook = new SpellBook();
-
         SpellBookIterator _spellBookIterator = new(new SpellBook().Spells);
         InventoryIterator _inventoryIterator = new(new Inventory().Items);
 
         void Awake()
         {
-            /*_getInventoryButton.onClick.AddListener(() => DisplayInventory(_inventory.Items));
-            _getSpellsButton.onClick.AddListener(() => DisplaySpellBook(_spellBook.Spells));*/
 
             _getInventoryButton.onClick.AddListener(() => DisplayAnything(_inventoryIterator));
             _getSpellsButton.onClick.AddListener(() => DisplayAnything(_spellBookIterator));
@@ -30,32 +25,10 @@ namespace DesignPatterns.Iterator
             _outputTextbox.text = string.Empty;
         }
 
-        void DisplayInventory(List<Item> inventory)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < inventory.Count; i++)
-            {
-                sb.Append(inventory[i].Name);
-                sb.Append("\n");
-            }
-
-            _outputTextbox.text = sb.ToString();
-        }
-
-        void DisplaySpellBook(Spell[] spells)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < spells.Length; i++)
-            {
-                sb.Append(spells[i].Name);
-                sb.Append("\n");
-            }
-
-            _outputTextbox.text = sb.ToString();
-        }
-
         void DisplayAnything(IIterator iterator)
         {
+            iterator.Reset();
+
             StringBuilder sb = new StringBuilder();
             while (iterator.HasNext())
             {
