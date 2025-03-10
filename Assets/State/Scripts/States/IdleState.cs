a0bca34040c3508cc723b1549df0321530c19d9c
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-09 20:02:01 -0400

    created states

---
+++ Assets/State/Scripts/States/IdleState.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace DesignPatterns.State
+{
+    public class IdleState : IState
+    {
+        private Material _mat;
+
+        public IdleState(Material mat)
+        {
+            _mat = mat;
+        }
+
+        public void Enter()
+        {
+            _mat.color = Color.blue;
+        }
+
+        public void Exit()
+        {
+            _mat.color = Color.white;
+        }
+
+        public void Tick()
+        {
+            //
+        }
+
+        public void FixedTick()
+        {
+            //
+        }
+    }
+}
