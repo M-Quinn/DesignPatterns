3895ba2a652d751be52a4c18149c416e385b0c48
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-20 11:52:36 -0400

    created prefabs and finished the scene

--- Assets/Bridge/Scripts/Weapon/LongSword.cs
+++ Assets/Bridge/Scripts/Weapon/LongSword.cs
@@ -1,6 +1,11 @@
+using DesignPatterns;
 using UnityEngine;
 
-public class LongSword
+public class LongSword : Weapon
 {
-    
+    public override void Attack()
+    {
+        Debug.Log("Long Sword is attacking!");
+        _weaponEffect.PlayEffect();
+    }
 }
