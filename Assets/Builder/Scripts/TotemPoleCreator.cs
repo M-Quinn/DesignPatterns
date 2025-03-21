76d867fa239fda92823c5d1fcb1be17ec2dc1fcc
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-03-21 09:36:20 -0400

    updated to show two

--- Assets/Builder/Scripts/TotemPoleCreator.cs
+++ Assets/Builder/Scripts/TotemPoleCreator.cs
@@ -11,10 +11,16 @@ namespace DesignPatterns.Builder
 
         void Start()
         {
-            GameObject totemObject = Instantiate(totemPrefab);
-            if (totemObject.TryGetComponent<TotemPole>(out var totemPole))
+            GameObject totemObject1 = Instantiate(totemPrefab, new Vector3(1,0,0), Quaternion.identity);            
+            GameObject totemObject2 = Instantiate(totemPrefab, new Vector3(-1,0,0), Quaternion.identity);
+
+            if (totemObject1.TryGetComponent<TotemPole>(out var totemPole))
             {
                 _totemDirector.ConstructTotem(new WoodenTotemPoleBuilder(totemPole), 2);
+            }
+            if (totemObject2.TryGetComponent<TotemPole>(out var totemPole1))
+            {
+                _totemDirector.ConstructTotem(new StoneTotemPoleBuilder(totemPole1), 2);
             }
         }
     }
